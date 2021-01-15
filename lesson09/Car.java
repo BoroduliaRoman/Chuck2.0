b8a4a9da03989b16300f425a630055d6c44dd4cf
BoroduliaRoman <56869382+BoroduliaRoman@users.noreply.github.com>
2021-01-15 22:40:07 +0200

    lesson 9

---
+++ lesson09/Car.java
@@ -0,0 +1,69 @@
+package lesson09;
+
+public class Car {
+	
+	final int XYZ = 3;
+	int zzzzzzzzzzzzzzzzzzzzzzzzzzz = 5;
+	int _Zvsgd = 15;
+	int ___________$$$$$$$$a4___________ = 10;
+	
+	
+	String color;
+	int a = 10;
+	String engine;
+	static int count;
+	
+//	public static void changeA(int b) {
+//		Car c = new Car("black", "V8");
+//		c.a = b;
+//	}
+	
+	Car(String color, String engine) {		
+//		int x = 5;
+//		int y = x + 10;
+//		this.count++;
+		int a = 5;
+		this.color = color;
+		this.engine = engine;		
+	}
+	
+//	public void showColor() {
+//		System.out.println("÷вет машины: " + color);
+////		int a = cena;
+//		changeColor("red");
+//	}
+	
+//	public void changeColor(String color3) {
+////		String a = color3;
+//		System.out.println("÷вет машины изменилс€");
+//		int cena = 5000;
+//		color = color3;
+//		cena += 1000;
+//	}
+	
+//	int a = cena;
+	
+//	void abc(int a, int b) {
+//		boolean c = true;
+//	}
+//	
+//	void abcd(int a, int b) {
+//		boolean c = true;
+//	}
+	
+	void changeColor(String color) {
+		this.color = color;
+	}
+	
+	public static void main(String[] args) {
+		
+		Car c = new Car("red", "V6");
+//		System.out.println(c.color);
+//		System.out.println(c.color);
+//		c.changeColor("black");
+		int a = 10;
+		a = a;
+		
+	}
+
+}
---
+++ lesson09/Student.java
@@ -0,0 +1,17 @@
+package lesson09;
+
+public class Student {
+
+}
+
+class StudentTest {
+	
+	public static void main(String[] args) {
+		
+		Student st1 = new Student();
+		Student st2 = new Student();
+		Student st3;
+		String s = "privet";
+		st1 = null;
+	}
+}
