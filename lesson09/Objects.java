3acb64a0683b08661e731c52b2514424d60334c3
BoroduliaRoman <56869382+BoroduliaRoman@users.noreply.github.com>
2021-01-15 22:40:39 +0200

    Homework 9

---
+++ lesson09/Objects.java
@@ -0,0 +1,25 @@
+package homework.lesson09;
+
+public class Objects {
+
+	public static void main(String[] args) {
+		
+		Objects o1 = new Objects();
+		Objects o2 = new Objects();
+		Objects o3 = new Objects();
+		Objects o4 = new Objects();
+		Objects o5 = new Objects();
+		Objects o6 = new Objects();
+		Objects o7 = new Objects();
+		Objects o8 = new Objects();
+		
+		o1 = null;
+		o2 = null;
+		o3 = null;
+		o4 = null;
+		o5 = null;
+		o6 = null;
+		
+	}
+
+}
