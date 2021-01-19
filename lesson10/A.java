4e091aba7a75a75c3f94ff611ad57be6a223649f
BoroduliaRoman <56869382+BoroduliaRoman@users.noreply.github.com>
2021-01-19 09:38:33 +0200

    lesson 10

---
+++ lesson10/A.java
@@ -0,0 +1,35 @@
+package lesson10;
+
+//import lesson09.*;
+//import lesson08.*;
+//import java.lang.*;
+
+//import homework.lesson04.*;
+import static lesson09.Car.*;
+
+public class A {
+	
+//	static int count = 99;
+	
+	public static void main(String[] args) {
+		
+//		lesson09.Car c1 = new lesson09.Car("red", "V6");
+//		lesson08.Car c2 = new lesson08.Car();
+//		lesson09.Student st1 = new lesson09.Student();
+		
+//		java.lang.String st1 = new java.lang.String("hello");
+//		Student st2 = new Student();
+//		System.out.println(st2.z);
+//		System.out.println(st2.c);
+		
+//		Student st1 = new Student();
+		
+		System.out.println(ab); // kod vivodit peremennuy na display 
+		System.out.println(count);
+		
+	}	
+}
+
+//class B {	
+//	
+//}
