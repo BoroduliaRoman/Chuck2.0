222e071bce24e686ccf22f4f3e940f1e76fe3f22
BoroduliaRoman <56869382+BoroduliaRoman@users.noreply.github.com>
2021-02-24 20:29:01 +0200

    lesson12

---
+++ lesson12/Test10.java
@@ -0,0 +1,92 @@
+package lesson12;
+
+public class Test10 {
+	
+//	void maximum(int i1, int i2, int i3) {
+//		if (i1 > i2 && i1 > i3) {
+//			int a = 5;
+//			System.out.println("Maximum - " + i1);
+//		} else if (i2 > i1 && i2 > i3) {
+//			int a = 10;
+//			System.out.println("Maximum - " + i2);
+//		} else {
+//			int a = 15;
+//			System.out.println("Maximum - " + i3);
+//		}
+//	}
+	
+	void abc() {		
+		String str;
+		int a = 5;
+		
+		if (a >= 10) {
+			str = "Privet";
+		}
+		
+		if (a < 10) {
+			str = "Poke";
+		} else {
+			str = "sdasf";
+		}
+		
+		System.out.println(str);
+	}
+	
+	public static void main(String[] args) {
+		
+		int a = 20;
+		int b = 20;
+		
+		int maximum = (a > b) ? a:b;
+		System.out.println(maximum);
+		
+//		Test10 t = new Test10();
+//		
+//		t.maximum(4, 7, 0);
+		
+//		int a = 10;
+//		int salary = 500;
+		
+//		if(salary >= 500);
+//		System.out.println("dgsdgklgjs");
+		
+//		if (salary >= 500) {
+//			System.out.println("a menshe 20");
+//			System.out.println("dsadfbjashf");
+//			a++;
+//			
+//			System.out.println(":)");
+//		} else {
+//			System.out.println("a NE menshe 20");	
+//			System.out.println("Privet vsem!!!");
+//			
+//			System.out.println(":(");
+//		}
+//		
+//		System.out.println("Vse budet horosho!");		
+		
+//		if (salary >= 500) {
+//			System.out.println(":)");
+//			System.out.println("!!!!!");
+//		} else {
+//			System.out.println(":(");
+//		}
+		
+//		int salary = 1500;
+//		boolean b = true;
+//		
+//		if (salary < 200) {
+//			System.out.println("Z/P ochen nizka");
+//		} else if (salary < 400 && b == false || true) {
+//			System.out.println("Z/P srednego razmera");
+//		} else if (b == true) {
+//			System.out.println("Z/P visokaya");
+//		} /* else {
+//			System.out.println("Z/P otlichnaya");
+//		} */
+		
+		
+		
+	}
+
+}
